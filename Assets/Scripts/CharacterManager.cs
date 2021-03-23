faefec827c5774f8d6692658bd50d99e78928a0c
JCela <cjc771@nyu.edu>
2021-03-23 18:29:06 -0400

    Added Claw animation, added fade animation

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -10,9 +10,11 @@ public class CharacterManager : MonoBehaviour
     private Vector2 thrust;
     public Tasks taskScript;
     
+    
 
     private IEnumerator killcoroutine;
     private IEnumerator addnewcharacter;
+    private IEnumerator throwup;
     public GameObject charPrefab;
     public int characterHeight = 1;
 
@@ -25,10 +27,12 @@ public class CharacterManager : MonoBehaviour
     private TagScript tag2script;
     private TagScript tag3script;
 
+    public Animator hookAnimator;
+    private Animator playerAnimator;
 
     void Start()
     {
-        thrust = new Vector2(40, 0);
+        thrust = new Vector2(0, 1);
         character = GameObject.FindWithTag("Character");
         uiManagerScript = this.GetComponent<UIManager>();
         tag1 = character.transform.GetChild(0).gameObject;
@@ -38,26 +42,31 @@ public class CharacterManager : MonoBehaviour
         tag1script = tag1.GetComponent<TagScript>();
         tag2script = tag2.GetComponent<TagScript>();
         tag3script = tag3.GetComponent<TagScript>();
+        
+
+
     }
      
     void Update()
     {
         killcoroutine = PushAndKill(1.0f);
         addnewcharacter = AddNewChar(1.0f);
+        throwup = PushUp(1.0f);
         character = GameObject.FindWithTag("Character");
         rb = character.GetComponent<Rigidbody2D>();
 
         soulPoints = tag1script.currentPoints + tag2script.currentPoints + tag3script.currentPoints;
         Debug.Log("This souls value in points is: " + soulPoints);
+        playerAnimator = character.GetComponent<Animator>();
     }
 
     public void SwipeCharacterRight()
     {
         Debug.Log("Player has chosen to send character to Ascend.");
+        //hookAnimator.SetTrigger("clawGrab");
         taskScript.Ascended++;
-        //rb.AddForce(thrust, ForceMode2D.Impulse);
-        StartCoroutine(killcoroutine);
-        StartCoroutine(addnewcharacter);
+        StartCoroutine(throwup);
+        
     }
 
     public void SwipeCharacterLeft()
@@ -65,14 +74,30 @@ public class CharacterManager : MonoBehaviour
         Debug.Log("Player has chosen to send character to Reincarnate.");
         taskScript.Descended++;
         //rb.AddForce(-thrust, ForceMode2D.Impulse);
+        playerAnimator.SetTrigger("Faded");
         StartCoroutine(killcoroutine);
         StartCoroutine(addnewcharacter);
     }
 
+    private IEnumerator PushUp(float waitTime)
+    {
+        hookAnimator.SetTrigger("clawGrab");
+        
+        yield return new WaitForSeconds(waitTime);
+        hookAnimator.ResetTrigger("clawGrab");
+        rb.AddForce(thrust, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(1.0f);
+        
+        StartCoroutine(killcoroutine);
+        StartCoroutine(addnewcharacter);
+        StopCoroutine(throwup);
+    }
     private IEnumerator PushAndKill(float waitTime)
     {
+        
         yield return new WaitForSeconds(waitTime);
         //Kill current character
+        //playerAnimator.SetTrigger("Fade");
         Destroy(character);
         StopCoroutine(killcoroutine);
     }
