132b625c2f7053efd94b4eb1251ddd951a2c8072
JunLin Li <jl8278@nyu.edu>
2021-03-24 17:16:36 +0800

    first attempt on name Generator

---
+++ Assets/Art/soul profile stuff/randomNameGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class randomNameGenerator : MonoBehaviour
+{
+    string[] firstName ={ "Jay", "Jackson", "Lynn", "Mark", "Zehua", };
+    string[] lastName = { "Maylol", "Coolie", "Whatt", "Wewe", "Wolly", };
+
+
+    public string GetRandomName()
+    {
+
+        int firstNameIndex = Random.Range(0, firstName.Length);
+        int lastNameIndex = Random.Range(0, lastName.Length);
+
+        string characterName =
+
+            firstName[firstNameIndex] +" "+
+            lastName[lastNameIndex];
+
+        return characterName;
+    }
+
+
+
+
+
+
+}
---
+++ Assets/Art/soul profile stuff/testNameScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testNameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var nameGenerator = GetComponent<randomNameGenerator>();
+        Debug.Log(nameGenerator.GetRandomName());
+    }
+}
