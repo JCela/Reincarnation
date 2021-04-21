f8e4e4b4208efe63b8f58dd35c0b54deeabff477
JCela <cjc771@nyu.edu>
2021-04-20 22:32:20 -0400

    Added Stretch animation cycle, added walking in front left, lerp is broken tho so needs fix,  fixed up menu, fixed boss chat bug, added fade in animation

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -14,6 +14,7 @@ public class CharacterManager : MonoBehaviour
 
     private IEnumerator killcoroutine;
     private IEnumerator addnewcharacter;
+    private IEnumerator lerpPlayer;
     private IEnumerator throwup;
     public GameObject charPrefab;
     public int characterHeight = 1;
@@ -23,13 +24,17 @@ public class CharacterManager : MonoBehaviour
     private GameObject tag3;
     public int soulPoints;
 
+    private float timeelapsed;
+    private float lerpduration = 3;
+
     private TagScript tag1script;
     private TagScript tag2script;
     private TagScript tag3script;
 
     public Animator hookAnimator;
     private Animator playerAnimator;
-
+    public Transform startMarker;
+    public Transform endMarker;
     void Start()
     {
         thrust = new Vector2(0, 1);
@@ -59,9 +64,12 @@ public class CharacterManager : MonoBehaviour
         tag3script = tag3.GetComponent<TagScript>();
         killcoroutine = PushAndKill(1.0f);
         addnewcharacter = AddNewChar(1.0f);
+        lerpPlayer = LerpPlayer(15000.0f);
         throwup = PushUp(1.0f);
         character = GameObject.FindWithTag("Character");
         rb = character.GetComponent<Rigidbody2D>();
+
+        
 
         soulPoints = tag1script.currentPoints + tag2script.currentPoints + tag3script.currentPoints;
         Debug.Log("This souls value in points is: " + soulPoints);
@@ -114,8 +122,17 @@ public class CharacterManager : MonoBehaviour
     {
         yield return new WaitForSeconds(waitTime);
         //Add a new character
-        Instantiate(charPrefab, new Vector3(-1, characterHeight, -1), Quaternion.identity);
+        Instantiate(charPrefab, new Vector3(-4, characterHeight, -1), Quaternion.identity);
+        StartCoroutine(lerpPlayer);
         StopCoroutine(addnewcharacter);
+    }
+
+    private IEnumerator LerpPlayer(float waitTime)
+    {
+        yield return new WaitForSeconds(1.0f);
+        character.transform.position = Vector3.Lerp(startMarker.position, endMarker.position, 1f);
+        yield return new WaitForSeconds(waitTime);
+        StopCoroutine(lerpPlayer);
     }
 
 }
